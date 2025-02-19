ac37ce93f2a729b0e9d54cbd0c77b4cc43eabbe3
marcelomengech <marcelomengech@gmail.com>
2025-02-19 22:03:27 +0300

    initial commit

---
+++ lib/functions.py
@@ -0,0 +1,17 @@
+
+#!/usr/bin/env python3
+
+def greet_programmer():
+    print("Hello, programmer!")
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
+
+def add(num1, num2):
+    return num1 + num2
+
+def halve(number):
+    return number / 2
